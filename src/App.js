cbf24410012c1411117f6a2ba3725cb50bacc2e0
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-20 17:18:49 -0500

    Update App.js
    Wrap components in sections

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/App.scss';
 import Header from './components/header';
 import Landing from './components/landing';
@@ -7,13 +8,21 @@ import Tools from './components/tools';
 import Process from './components/process';
 
 const App = () => (
-	<div>
-		<Header />
-		<Landing />
-		<About />
-		<Tools />
-		<Process />
-		<div className="footer">© Jenna Michaels 2020</div>
-	</div>
+	<Router>
+		<div>
+			<Header />
+			<Landing />
+			<section id="about">
+				<About />
+			</section>
+			<section id="tools">
+				<Tools />
+			</section>
+			<section id="process">
+				<Process />
+			</section>
+			<div className="footer">© Jenna Michaels 2020</div>
+		</div>
+	</Router>
 );
 export default App;
