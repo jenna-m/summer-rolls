0ab82157befbbdd7205d2e000eee710aff9f1e65
Jenna M <19200395+jenna-m@users.noreply.github.com>
2020-01-20 16:31:23 -0500

    Converting to React App
    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.scss';
+import Header from './components/header';
+import Landing from './components/landing';
+import About from './components/about';
+import Tools from './components/tools';
+import Process from './components/process';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App = () => (
+	<div>
+		<Header />
+		<Landing />
+		<About />
+		<Tools />
+		<Process />
+		<div className="footer">© Jenna Michaels 2020</div>
+	</div>
+);
 export default App;
